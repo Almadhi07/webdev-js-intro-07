512135dedc10579601a41f7d8ceb5d47105e43fd
Almadhi07 <aaalma05@louisville.edu>
2025-01-07 16:11:38 -0500

    I declared functions that use to calculate the remainder and check if number is odd or even and declared arrays.

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,30 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+
+function returnTheRemainder (num1, num2){
+    return num1 % num2;
+}
+
+function checkIfEven(num){
+    return num % 2 === 0;
+
+}
+
+function checkIfEven(num){
+    return num % 2 === 0;
+}
+
+function getTheFourthElement(arr){
+    return arr[3];
+}
+
+submissionBtn.addEventListener("click", function(){
+    remainderElement.textContent = returnTheRemainder(5,4);
+
+    checkIfEvenElement.textContent = checkIfEven(47);
+
+    lostNumbersElement.textContent = getTheFourthElement(lostNumbers);
+
+});
